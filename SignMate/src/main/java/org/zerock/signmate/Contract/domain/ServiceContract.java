b367908a43f0e9caa6543c0800aa151dcae26869
spp2391 <spp2391@naver.com>
2025-08-11 14:07:52 +0900

    20250811 커밋

--- SignMate/src/main/java/org/zerock/signmate/Contract/domain/ServiceContract.java
+++ SignMate/src/main/java/org/zerock/signmate/Contract/domain/ServiceContract.java
@@ -13,52 +13,41 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @Builder
 public class ServiceContract extends CommonEntity{
-
+    // 공통적인 칼럼들
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)  // 자동 증가 PK
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "contract_id")
     private Contract contract;
-
     private String clientName;
     private String projectName;
-
     private LocalDate contractStartDate;
     private LocalDate contractEndDate;
+    private LocalDate deliveryDeadline;
+    private LocalDate contractDate;
+    @Enumerated(EnumType.STRING)
+    private enums.ContractStatus status; // DRAFT, PENDING, SIGNED
+    @Column(length = 255)
+    private String url;  // 계약서 고유 링크 URL 저장
 
+
+    // 개별적인 칼럼들
     private String totalAmount;
     private String advancePayment;
     private String interimPayment;
     private String finalPayment;
-
     @Column(columnDefinition = "TEXT")
     private String paymentTerms;
-
     private Boolean taxInvoice;
     private String paymentMethod;
-
     @Column(columnDefinition = "TEXT")
     private String workDescription;
     private Boolean deliverOriginalFiles;
-
     private Integer revisionCount;
-    private LocalDate deliveryDeadline;
-
     @Column(columnDefinition = "TEXT")
     private String otherNotes;
-
-    @Enumerated(EnumType.STRING)
-    private enums.ContractStatus status; // DRAFT, PENDING, SIGNED
-
-    private String pdfPath; // 최종 PDF 경로
-
-    private LocalDate contractDate;
-
-    @Column(length = 255)
-    private String url;  // 계약서 고유 링크 URL 저장
-
 
 }
 
